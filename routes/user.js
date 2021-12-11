556cadd852d62dbd1b2c2c6ffa9ded246a885394
Justin Williams <justinlamarr1992@icloud.com>
2021-12-10 19:29:31 -0600

    testing for user

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,9 @@
 const express = require("express");
-
 const router = express.Router();
-
 // routes;
 router.get("/user", (req, res) => {
   res.json({
     data: "test 1,2,3",
   });
 });
-
 module.exports = router;
